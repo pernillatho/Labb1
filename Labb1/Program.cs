a8cad51b5134638a5f953d6a07abc3a8730c011d
pernillatho <112381808+pernillatho@users.noreply.github.com>
2022-09-14 20:15:18 +0200

    First commit

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -2,8 +2,7 @@
 using System.Xml.Schema;
 
 Console.WriteLine("Skriv in valfri sträng:");
-//string input = Console.ReadLine();
-//Console.Clear();    
+//string input = Console.ReadLine();  
 string input = "29535123p48723487597645723645";
 
     for (int i = 0; i < input.Length; i++)
@@ -12,29 +11,30 @@ string input = "29535123p48723487597645723645";
 
         if (char.IsDigit(tecken))
         {
-            char startNumber = tecken;
-            string delsträng = startNumber.ToString();
-
-            int indexPositionJ = i + 1;
+            //char startNumber = tecken;
+            //string delsträng = startNumber.ToString();
+            string delsträng = tecken.ToString();
            
             for (int j = i + 1; j < input.Length; j++)
             {
                 char deltecken = input[j];
+                
                 if (char.IsLetter(deltecken))
                 {
                     break;
                 }
-                if (deltecken != startNumber)
-                {
-                    delsträng += deltecken;
-                }
-                else
-                {
+
+            if (deltecken != tecken/*startNumber*/)
+            {
+                delsträng += deltecken;
+            }
+
+            else
+            {
                     delsträng += deltecken;
 
-                    string startsträng = input.Substring(0, indexPositionJ - 1);
-                    int slutIndex = startsträng.Length + delsträng.Length;
-                    string slutsträng = input.Substring(slutIndex);
+                    string startsträng = input.Substring(0, i);
+                    string slutsträng = input.Substring(startsträng.Length + delsträng.Length);
 
                     Console.Write(startsträng);
                     Console.ForegroundColor = ConsoleColor.Magenta;
